fccd5db8da0dc7e91c9ccf44f91d0a88d921ac73
JoseRizo76 <147220868+JoseRizo76@users.noreply.github.com>
2024-05-12 21:01:29 -0600

    Add files via upload

---
+++ prueba2/Firebase/token.h
@@ -0,0 +1,18 @@
+/* FIREBASE DATOS */
+#define WIFI_SSID "POCO-F3"
+#define WIFI_PASSWORD "14122022"
+#define API_KEY "AIzaSyBStVNCahF00hNSd-SrgI8RbDRJm4Z6PrM" 
+#define USER_EMAIL "joseluisrizo7606@gmail.com"
+#define USER_PASSWORD "00000000"
+#define DATABASE_URL "https://esp32-7eeb0-default-rtdb.firebaseio.com/"
+
+// FIREBASE DATOS DE AUTENTICACION DE LA PAGINA
+FirebaseData fbdo;
+FirebaseAuth auth;
+FirebaseConfig config;
+
+//WETATHER DATOS
+String URL = "http://api.openweathermap.org/data/2.5/weather?&units=metric&";
+String ApiKey = "be39e66936f97a3c638ebd63508f47ca";
+String lon = "-86.034024";
+String lat = "11.392695";
